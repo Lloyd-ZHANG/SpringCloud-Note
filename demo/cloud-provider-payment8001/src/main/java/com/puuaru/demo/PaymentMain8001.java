1a476de05197d1789781dc63acc305816d64d122
AsakiAmane <honghuizhang4w@gmail.com>
2022-09-26 17:09:21 +0800

    update demo: payment service 8001

---
+++ demo/cloud-provider-payment8001/src/main/java/com/puuaru/demo/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.puuaru.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: Payment Service 8001 Application
+ * @Author: puuaru
+ * @Date: 2022/9/26
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+            SpringApplication.run(PaymentMain8001.class, args);
+        }
+}
